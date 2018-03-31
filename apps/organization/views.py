db2c9cf4dfd55bf65e8454d5189ab3cec1d2c4da
yyyooo <yangpu0001@gmail.com>
2018-04-01 04:36:27 +0800

    2018-4-1

--- apps/organization/views.py
+++ apps/organization/views.py
@@ -3,9 +3,11 @@ from django.views.generic import View
 from .models import CourseOrg, CityDict
 from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
 from organization.forms import UserAskForm
+from organization.models import Teacher
 from django.http import HttpResponse
 from courses.models import Course
 from operation.models import UserFavorite
+from django.db.models import Q
 
 
 class OrgView(View):
@@ -15,7 +17,13 @@ class OrgView(View):
     def get(self, request):
         all_orgs = CourseOrg.objects.all()
         hot_orgs = all_orgs.order_by("-click_nums")[:3]
+        #城市
         all_citys = CityDict.objects.all()
+
+        # 机构搜索
+        search_keywords = request.GET.get('keywords', "")
+        if search_keywords:
+            all_orgs = all_orgs.filter(Q(name__icontains=search_keywords) | Q(desc__icontains=search_keywords))
 
         #取出筛选城市
         city_id = request.GET.get('city', "")
@@ -179,7 +187,72 @@ class AddFavView(View):
 
 
 
+class TeacherListView(View):
+    """
+    课程讲师列表页
+    """
+    def get(self, request):
+        all_teachers = Teacher.objects.all()
 
+        current_nav = "teacher"
+
+        #课程讲师搜索
+        search_keywords = request.GET.get('keywords', "")
+        if search_keywords:
+            all_teachers = all_teachers.filter(Q(name__icontains=search_keywords)|
+                                               Q(work_company__icontains=search_keywords)|
+                                               Q(work_position__icontains=search_keywords))
+
+        sort = request.GET.get('sort', "")
+        if sort:
+            if sort == "hot":
+                all_teachers = all_teachers.order_by("-click_nums")
+
+        sorted_teacher = Teacher.objects.all().order_by("-click_nums")[:3]
+
+        #对讲师进行分页
+        try:
+            page = request.GET.get('page', 1)
+        except PageNotAnInteger:
+            page = 1
+
+        p = Paginator(all_teachers, 1, request=request)
+
+        teachers = p.page(page)
+        return render(request, "teachers-list.html", {
+            "all_teachers":teachers,
+            "sorted_teacher":sorted_teacher,
+            "sort":sort,
+            "current_nav": current_nav
+        })
+
+
+class TeacherDetailView(View):
+    def get(self, request, teacher_id):
+        teacher = Teacher.objects.get(id=int(teacher_id))
+        teacher.click_nums += 1
+        teacher.save()
+        all_courses = Course.objects.filter(teacher=teacher)
+
+        has_teacher_faved = False
+        if request.user.is_authenticated:
+            if UserFavorite.objects.filter(user=request.user, fav_type=3, fav_id=teacher.id):
+                has_teacher_faved = True
+
+        has_org_faved = False
+        if request.user.is_authenticated:
+            if UserFavorite.objects.filter(user=request.user, fav_type=2, fav_id=teacher.org.id):
+                has_org_faved = True
+
+        #讲师排行
+        sorted_teacher = Teacher.objects.all().order_by("-click_nums")[:3]
+        return render(request, "teacher-detail.html", {
+            "teacher":teacher,
+            "all_courses":all_courses,
+            "sorted_teacher":sorted_teacher,
+            "has_teacher_faved": has_teacher_faved,
+            "has_org_faved":has_org_faved
+        })
 
 
 
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,13 +1,14 @@
+import json
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.hashers import make_password
 from django.views.generic.base import View
-from .forms import LoginForm, RegisterForm
+from django.http import HttpResponse
+
+from .forms import LoginForm, RegisterForm, UploadImageForm, ModifyPwdForm
 from .models import UserProfile, EmailVerifyRecord
 from utils.email_send import send_register_email
-
-# Create your views here.
-
+from utils.mixin_utils import LoginRequiredMixin
 
 # def user_login(request):
 #     if request.method == 'POST':
@@ -24,8 +25,9 @@ from utils.email_send import send_register_email
 
 
 class LoginView(View):
-    def get(self, request: object) -> object:
+    def get(self, request):
         return render(request, "login.html", {})
+
     def post(self,request):
         login_form = LoginForm(request.POST)
         if login_form.is_valid():
@@ -45,7 +47,7 @@ class LoginView(View):
 
 
 class RegisterView(View):
-    def get(self, request: object) -> object:
+    def get(self, request):
         register_form = RegisterForm()
         return render(request, "register.html", {'register_form':register_form})
 
@@ -67,7 +69,7 @@ class RegisterView(View):
             return render(request, "register.html", {"register_form":register_form})
 
 class ActiveUserView(View):
-    def get(self, request: object, active_code: object) -> object:
+    def get(self, request: object, active_code):
         all_records = EmailVerifyRecord.objects.filter(code = active_code)
         if all_records:
             for record in all_records:
@@ -79,6 +81,70 @@ class ActiveUserView(View):
         return render(request, "login.html")
 
 
+class UserinfoView(LoginRequiredMixin, View):
+    """
+    用户个人信息
+    """
+    def get(self, request):
+        return render(request, 'usercenter-info.html', {})
 
+    # def post(self, request):
+    #     user_info_form = UserInfoForm(request.POST, instance=request.user)
+    #     if user_info_form.is_valid():
+    #         user_info_form.save()
+    #         return HttpResponse('{"status":"success"}', content_type='application/json')
+    #     else:
+    #         return HttpResponse(json.dumps(user_info_form.errors), content_type='application/json')
+    #
 
+class UploadImageView(LoginRequiredMixin, View):
+    """
+    用户修改头像
+    """
+    def post(self, request):
+        image_form = UploadImageForm(request.POST, request.FILES, instance=request.user)
+        if image_form.is_valid():
+            image = image_form.cleaned_data['image']
+            return HttpResponse('{"status":"success"}', content_type='application/json')
+        else:
+            return HttpResponse('{"status":"fail"}', content_type='application/json')
 
+class ModifyPwdView(View):
+    """
+    修改用户密码
+    """
+    def post(self, request):
+        modify_form = ModifyPwdForm(request.POST)
+        if modify_form.is_valid():
+            pwd1 = request.POST.get("password1", "")
+            pwd2 = request.POST.get("password2", "")
+            email = request.POST.get("email", "")
+            if pwd1 != pwd2:
+                return render(request, "password_reset.html", {"email":email, "msg":"密码不一致"})
+            user = UserProfile.objects.get(email=email)
+            user.password = make_password(pwd2)
+            user.save()
+
+            return render(request, "login.html")
+        else:
+            email = request.POST.get("email", "")
+            return render(request, "password_reset.html", {"email":email, "modify_form":modify_form})
+
+class UpdatePwdView(View):
+    """
+    个人中心修改用户密码
+    """
+    def post(self, request):
+        modify_form = ModifyPwdForm(request.POST)
+        if modify_form.is_valid():
+            pwd1 = request.POST.get("password1", "")
+            pwd2 = request.POST.get("password2", "")
+            if pwd1 != pwd2:
+                return HttpResponse('{"status":"fail","msg":"密码不一致"}', content_type='application/json')
+            user = request.user
+            user.password = make_password(pwd2)
+            user.save()
+
+            return HttpResponse('{"status":"success"}', content_type='application/json')
+        else:
+            return HttpResponse(json.dumps(modify_form.errors), content_type='application/json')
